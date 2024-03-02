ac898cdf29a7bee3a00668395781b3654f3de5d9
Jayesh Rakesh Deore <153099744+jayeshrdeore@users.noreply.github.com>
2024-03-02 06:31:36 +0530

    Add files via upload

---
+++ js/hms.js
@@ -0,0 +1,300 @@
+// booking html & css file in js
+function abc(x)
+{
+    x.style.background = "#E89E00";
+}
+function fn()
+{
+  var x = document.getElementById("FN");
+  x.value=x.value.toUpperCase();
+}
+function fn2()
+{
+  var x = document.getElementById("LN");
+  x.value=x.value.toUpperCase();
+}
+function sdf()
+{
+  x.style.background = "green";
+}
+function proceed()
+{
+  var x = document.getElementById("FN").value;
+  var y = document.getElementById("LN").value;
+  if(x==""||y=="")
+  {
+    alert("KINDLY FILL ALL THE DETAILS");
+  }
+  else
+  {
+    alert("hello "+x+" "+y+" successfully submited");
+  }
+}
+
+// directory html & css file in js
+function a()
+{
+  alert("!!! Cant't connect at the moment !!!");
+}
+function b()
+{
+  alert("Line Busy... Try Again Later");
+}
+
+// forget html css file in js
+function login()
+{
+var a=document.getElementById('username').value
+var b=document.getElementById('password').value
+if(a==""||b=="")
+{
+alert("kindly fill all the details");
+}
+else
+alert("an OTP has been sent on "+b);
+}
+
+// gamebook html css file in js
+function abc(x)
+{
+  x.style.background = "#E89E00";
+}
+function sdf()
+{
+  x.style.background = "green";
+}
+
+//hms.html file in js
+function AV()
+{
+  alert(" :-) YOU ARE ON HOME PAGE :-)");
+}
+function abc()
+{
+  var a = prompt("enter guest ID")
+  if(a=="")
+{
+  alert("please enter guest ID");
+}
+else
+if(a=="ABCD")
+window.open("guest.html");
+else
+alert("invalid guest id");
+}
+
+// login html css file in js
+function login()
+{
+var a=document.getElementById('username').value
+var b=document.getElementById('password').value
+if(a==""||b=="")
+{
+alert("!!! please fill the details !!!");
+}
+else 
+if(a=="SAVENUE" && b=="12345")
+{
+window.open("manager.html");
+}
+else
+if(a=="GAVENUE" && b=="12345")
+{
+window.open("gservice.html");
+}
+else
+if(a=="RJGA" && b=="12345")
+{
+window.open("manager.html");
+}
+else
+if(a=="ABVE" && b=="12345")
+{
+window.open("manager.html");
+}
+else
+if(a=="ABZR" && b=="12345")
+{
+window.open("manager.html");
+}
+else
+if(a=="AMNM" && b=="12345")
+{
+window.open("manager.html");
+}
+else
+if(a=="YASW" && b=="12345")
+{
+window.open("manager.html");
+}
+else
+{
+alert("wrong id/password");
+}
+}
+
+
+// login1 html css file in js
+function login()
+{
+var a=document.getElementById('username').value
+var b=document.getElementById('password').value
+if(a==""||b=="")
+{
+alert("!!! please fill the details !!!");
+}
+else 
+if(a=="SAVENUE" && b=="12345")
+{
+window.open("manager1.html");
+}
+else
+if(a=="GAVENUE" && b=="12345")
+{
+window.open("gservice.html");
+}
+else
+if(a=="RJGA" && b=="12345")
+{
+window.open("manager1.html");
+}
+else
+if(a=="ABVE" && b=="12345")
+{
+window.open("manager1.html");
+}
+else
+if(a=="ABZR" && b=="12345")
+{
+window.open("manager1.html");
+}
+else
+if(a=="AMNM" && b=="12345")
+{
+window.open("manager1.html");
+}
+else
+if(a=="YASW" && b=="12345")
+{
+window.open("manager1.html");
+}
+else
+{
+alert("wrong id/password");
+}
+}
+
+//manager html css file in js
+function abc()
+{
+var a=prompt("confirm password");
+if (a=="12345")
+window.open("staff.html");
+else
+alert("INVALID PASSWORD/////ACCESS DENIED");
+}
+function xyz()
+{
+var a=prompt("confirm password");
+if (a=="12345")
+window.open("account.html");
+else
+alert("INVALID PASSWORD/////ACCESS DENIED");
+}
+function abc1()
+{
+var a=prompt("confirm password");
+if (a=="12345")
+window.open("rooms.html");
+else
+alert("INVALID PASSWORD/////ACCESS DENIED");
+}
+function abc2()
+{
+var a=prompt("confirm password");
+if (a=="12345")
+window.open("catering.html");
+else
+alert("INVALID PASSWORD/////ACCESS DENIED");
+}
+
+
+//new html css file in js
+function abc(x) 
+{
+  x.style.background = "#E89E00";
+}
+function fn()
+{
+var x = document.getElementById("FN");
+x.value=x.value.toUpperCase();
+}
+function fn2()
+{
+var x = document.getElementById("LN");
+x.value=x.value.toUpperCase();
+}
+function sdf()
+{
+x.style.background = "green";
+}
+function proceed()
+{
+var x = document.getElementById("FN").value;
+var y = document.getElementById("LN").value;
+if(x==""||y=="")
+{
+alert("KINDLY FILL ALL THE DETAILS");
+}else
+alert("hello "+x+" "+y+" successfully created your ACCOUNT");
+}
+
+
+// renew html css file in js
+function abc(x) {
+  x.style.background = "#E89E00";
+}
+
+function fn()
+{
+var x = document.getElementById("FN");
+x.value=x.value.toUpperCase();
+}
+
+function fn2()
+{
+var x = document.getElementById("LN");
+x.value=x.value.toUpperCase();
+}
+
+function sdf()
+{
+x.style.background = "green";
+}
+
+function proceed()
+{
+var x = document.getElementById("FN").value;
+if(x=="")
+{
+alert("KINDLY FILL ALL THE DETAILS");
+}else
+alert("hello Mr. Alpha successfully renewed your services");
+}
+
+
+// rservice html css file in js
+function abc()
+{
+alert("we will be available to you shortly...THANK YOU!!");
+}
+
+// trans html css file in js
+function abc(x) {
+  x.style.background = "#E89E00";
+
+}
+function sdf()
+{
+x.style.background = "green";
+}
